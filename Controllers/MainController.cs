1d8ba06b04950e810edd8a57ca22bb594e805d08
Waldemar Rogalski <Rogalskiwaldemar@web.de>
2023-06-03 06:33:21 +0200

    Add MainController for retrieving the newest manga list
    - Create a new controller named MainController.
    
    - Inject the MainService dependency into the controller.
    
    - Implement an action method named GetNewestMangaList using the HTTP GET attribute.
    
    - Use the MainService to fetch the newest manga list asynchronously.
    
    - Return the obtained list of MangaDataModel objects from the action method.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,24 @@
+﻿using MangaLibra_Scrape_API.Models;
+using MangaLibra_Scrape_API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangaLibra_Scrape_API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MainController : Controller
+	{
+		private readonly MainService _mainService;
+        public MainController(MainService mainService)
+        {
+			_mainService = mainService;
+        }
+
+        [HttpGet(nameof(GetNewestMangaList))]
+		public async Task<List<MangaDataModel>> GetNewestMangaList()
+		{
+			return await _mainService.GetNewestMangaList();
+        }
+	}
+}
