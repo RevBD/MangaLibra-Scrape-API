3f2c2e37698c023f8f980e5cfc609f5c6fc66fad
Waldemar Rogalski <Rogalskiwaldemar@web.de>
2023-06-03 06:26:04 +0200

    Init _Forgot_

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using MangaLibra_Scrape_API.Models;
+using MangaLibra_Scrape_API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<MainService>();
+builder.Services.Configure<OptionsModel>(builder.Configuration.GetSection("SUPER_SECRET_DATA"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
