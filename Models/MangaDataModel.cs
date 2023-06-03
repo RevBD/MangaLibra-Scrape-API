3a52d75f79fb10f04fa6701f7afe2fe1511c96ff
Waldemar Rogalski <Rogalskiwaldemar@web.de>
2023-06-03 06:27:41 +0200

    Added MangaDataModel
    created Models Folder & First Model to Store Manga Data

---
+++ Models/MangaDataModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MangaLibra_Scrape_API.Models
+{
+    public class MangaDataModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Chapters { get; set; }
+        public string ImageLink { get; set; }
+        public string MangaLink { get; set; }
+        public string MangaId { get; set;}
+    }
+}
