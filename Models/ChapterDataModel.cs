e7b40663a72b9088e8df2e3a9a94b43bd41ccf4f
Rogalski Waldemar <55808527+RevBD@users.noreply.github.com>
2024-01-01 11:54:03 +0100

    Added Chapter Model

---
+++ Models/ChapterDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MangaLibra_Scrape_API.Models
+{
+    public class ChapterDataModel
+    {
+        public string? ChapterName { get; set; }
+        public string? ChapterLink { get; set; }
+        public string? ChapterDate { get; set; }
+    }
+}
