1ff23c2727d101f4d9d1887fbe3fb0b7e78f5f14
Rogalski Waldemar <55808527+RevBD@users.noreply.github.com>
2024-01-01 11:54:27 +0100

    Rewritten MangaDataModel

--- Models/MangaDataModel.cs
+++ Models/MangaDataModel.cs
@@ -2,11 +2,12 @@
 {
     public class MangaDataModel
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Chapters { get; set; }
-        public string ImageLink { get; set; }
-        public string MangaLink { get; set; }
-        public string MangaId { get; set;}
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Chapters { get; set; }
+        public string? ImageLink { get; set; }
+        public string? MangaLink { get; set; }
+        public string? MangaId { get; set;}
+        public List<ChapterDataModel>? ChapterList { get; set; }
     }
 }
